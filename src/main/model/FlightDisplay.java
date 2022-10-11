d7d6aa4b137823fbc92102434107312498be712e
Mohammad Fakhir <mfakhir@student.ubc.ca>
2022-10-11 13:07:59 -0700

    Renamed FlightList to FlightDisplay. Clarified functions performed by this class in comment after class declaration. Task 1 seems to be completed

---
+++ src/main/model/FlightDisplay.java
@@ -0,0 +1,103 @@
+package model;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class FlightDisplay {
+    /*
+     * TODO PURPOSE Ask user to add/remove flights to this since
+     *       I don't want to parse from file
+     * TODO: CONSIDER ADDING AFTER IMPLEMENTATION OF OTHERS: add emergency passenger alert section
+     * TODO: take inspiration from the Teller application
+     * possible things to use this list for:
+     *   present options to user:
+     *     to modify contents of the list of BOTH types of lists,
+     *     to modify/update fields of individual Flights in both lists
+     * todo later...........................: summary statistics
+     */
+
+    protected List<ArrivingFlight> arrivingFlights;
+    protected List<DepartingFlight> departingFlights;
+    private List<String> emergencyAlerts;
+
+    public FlightDisplay() {
+        runFlightDisplay();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: Runs the application which will set up the FlightDisplay using information from user
+    public void runFlightDisplay() {
+        /*
+         * 1. Initialize setup
+         * 2. Present options to user: ---- METHOD displayOptions()
+         *      - add/remove/update arrivingFlight METHOD to add + remove + update status
+         *      - add/remove/update departingFlight METHOD to add + remove + update status
+         *      - add/remove emergency alerts METHOD to add + remove emergency alerts
+         *      - end/quit program ???
+         * 3. Display/Print FlightDisplay after user chooses an option other than quitting
+         */
+        boolean isDisplayNeeded;
+        String option;
+        init();
+
+
+
+    }
+
+    // MODIFIES: Fields of the class FlightList
+    // EFFECTS: Initializes lists of Flights and emergency Alerts
+    public void init() {
+        // stub
+    }
+
+    // EFFECTS: Displays actions user can perform
+    public void displayOptionsMenu() {
+        // stub
+    }
+
+
+    public void addEmergencyAlert(String alert) {
+        emergencyAlerts.add(alert);
+    }
+
+    // not sure if this might be needed
+    public void endProgram() {
+        // stub
+        // not sure if this is needed and how will it function
+    }
+
+    // REQUIRES: alert passed to function must already be contained in the emergencyAlerts list
+    // MODIFIES: List<String> emergencyAlerts
+    // EFFECTS: removes specified alert
+    public void removeEmergencyAlert(String alert) {
+        emergencyAlerts.remove(alert);
+    }
+
+    // REQUIRES: a flight not already in the list
+    // MODIFIES: this
+    // EFFECTS: adds an arriving flight to the list
+    public void addArrivingFlight(ArrivingFlight flight) {
+        arrivingFlights.add(flight);
+    }
+
+    // REQUIRES: a flight already in the list
+    // MODIFIES: this
+    // EFFECTS: removes an arriving flight to the list
+    public void removeArrivingFlight(ArrivingFlight flight) {
+        arrivingFlights.remove(flight);
+    }
+
+    // REQUIRES: a flight not already in the list
+    // MODIFIES: this
+    // EFFECTS: adds a departing flight to the list
+    public void addDepartingFlight(DepartingFlight flight) {
+        departingFlights.add(flight);
+    }
+
+    // REQUIRES: a flight already in the list
+    // MODIFIES: this
+    // EFFECTS: removes a departing flight to the list
+    public void removeDepartingFlight(DepartingFlight flight) {
+        departingFlights.remove(flight);
+    }
+}
