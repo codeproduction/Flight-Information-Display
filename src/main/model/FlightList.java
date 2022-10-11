2fd38ff905c73f78fd6c5cf987265c04cace5f55
Mohammad Fakhir <mfakhir@student.ubc.ca>
2022-10-10 23:32:43 -0700

    Minor additions/edits to comments identifying functions performed by classes/methods

--- src/main/model/FlightList.java
+++ src/main/model/FlightList.java
@@ -5,19 +5,34 @@ import java.util.List;
 
 public class FlightList {
     /*
+     * TODO: CONSIDER ADDING AFTER IMPLEMENTATION OF OTHERS: add emergency passenger alert section
      * TODO: take inspiration from the Teller application
      * TODO: determine the function of this class: Ask user to add/remove flights to this since
      *       I don't want to parse from file
+     * todo later...........................: summary statistics
      * possible things to use this list for:
-     * present options to user:
+     *   present options to user:
      *     to modify contents of the list,
      *     to modify fields of Flights in the list
-     * make two separate lists for arriving and departing flight which could then be used separately in the different
-     * departure and arrival sections on the display.
      */
 
-    protected List<Flight> arrivingFlights = new ArrayList<>();
-    protected List<Flight> departingFlights = new ArrayList<>();
+    protected List<ArrivingFlight> arrivingFlights = new ArrayList<>();
+    protected List<DepartingFlight> departingFlights = new ArrayList<>();
+
+    // MODIFIES: this
+    // EFFECTS: runs the application which will set up the FlightDisplay using information from user
+    public void runFlightDisplay() {
+        /*
+         * 1. Display/Print FlightDisplay         *
+         * 2. Present options to user:
+         *      - add/remove/update arrivingFlight
+         *      - add/remove/update departingFlight
+         *      - add/remove emergency alerts
+         *      - end/quit program ???
+         */
+    }
+
+
 
     // REQUIRES: a flight not already in the list
     // MODIFIES: this
@@ -36,14 +51,14 @@ public class FlightList {
     // REQUIRES: a flight not already in the list
     // MODIFIES: this
     // EFFECTS: adds a departing flight to the list
-    public void addDepartingFlight(ArrivingFlight flight) {
-        arrivingFlights.add(flight);
+    public void addDepartingFlight(DepartingFlight flight) {
+        departingFlights.add(flight);
     }
 
     // REQUIRES: a flight already in the list
     // MODIFIES: this
     // EFFECTS: removes a departing flight to the list
-    public void removeDepartingFlight(ArrivingFlight flight) {
-        arrivingFlights.remove(flight);
+    public void removeDepartingFlight(DepartingFlight flight) {
+        departingFlights.remove(flight);
     }
 }
